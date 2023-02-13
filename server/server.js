56a727f1501c7ea3061e0d149b6b87ff0fcf1a8d
jay <jayp33233@gmail.com>
2023-02-13 19:23:15 +0530

    backend -> server/  bug fixed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/camhive", {
+mongoose.connect("mongodb://0.0.0.0:27017/camhive", {
    useNewUrlParser: true,
    useUnifiedTopology: true
 });

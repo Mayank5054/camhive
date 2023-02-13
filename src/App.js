86d057579e1381b7a0c6b7f39e510526053fc671
Mayank <mayanksheladiya49@gmail.com>
2023-02-13 23:32:35 +0530

    linkes restructed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home01 from './components/home01';
 import OtpPage from './components/OtpPage';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home01 />} />
-          <Route exact path="/otp" element={<OtpPage />} />
+          <Route exact path="/otp" element={<OtpPage />}/>
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>

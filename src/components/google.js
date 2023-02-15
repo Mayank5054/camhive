5d65ac3eea606d66b76dc6382dca346f2c51e7b0
Mayank <mayanksheladiya49@gmail.com>
2023-02-15 08:56:47 +0530

    successfully attempted google integration

--- src/components/google.js
+++ src/components/google.js
@@ -6,6 +6,7 @@ function Google() {
   var refe = useRef();
   var [isLogIn, setLogIn] = useState(false);
   var [data, setData] = useState({});
+  var [data_personal,setPersonal]=useState({})
   const obj = window.gapi;
 
   var CLIENT_ID =
@@ -14,7 +15,10 @@ function Google() {
     // var CLIENT_ID ="355367033227-g3rr36a02g06lrev47m1fa94nc7k8fle.apps.googleusercontent.com";
     // var API_KEY = "AIzaSyAR18WEx2u15IrGzRRYgXTslGGRKWwTnoM";
 
-  const SCOPES = "https://mail.google.com/ " + " https://www.googleapis.com/auth/user.birthday.read";
+  const SCOPES = "https://mail.google.com/ " + 
+  " https://www.googleapis.com/auth/user.birthday.read " +
+  " https://www.googleapis.com/auth/user.gender.read " +
+  " https://www.googleapis.com/auth/user.addresses.read "
   obj.load("client:auth2", async () => {
     obj.client.init({
       apiKey: API_KEY,
@@ -26,25 +30,37 @@ function Google() {
   const handleCal = async () => {
     var dov = window.gapi.auth2.getAuthInstance();
     if (isLogIn === false) {
-      await dov.signIn().then((e) => {
+      await dov.signIn().then(async (e) => {
         var db = e.qv;
-        setData(() => {
-          return db;
-        });
+        await setData(()=>{return db;});
+        console.log("data = ");
         console.log(data);
-        console.log(e);
+        // console.log(e);
         // console.log(window.gapi.auth2);
-        console.log(e.getAuthResponse());
+        // console.log(e.getAuthResponse());
       });
     }
     const state = await window.gapi.auth2.getAuthInstance().isSignedIn.Oa;
     setLogIn(state);
 
+  await window.gapi.client.load("people","v1",async ()=>{
     var req01=window.gapi.client.people.people.get({
-      'resourceName': 'me',
-            'personFields': 'genders,birthdays',})
+      'resourceName': 'people/me',
+            'personFields': 'birthdays,genders,addresses'})
     console.log(req01);
-    req01.execute((e)=>{console.log('reqo1 = ' );console.log(e);})
+    req01.execute(async (e)=>{
+    console.log("data1 = ");
+    console.log(data);
+    await setPersonal(
+      {
+        birthday:e.birthdays[0].date,
+        gender:e.genders[0].value});
+  })
+  console.log(data);
+  console.log(data_personal)
+
+  })
+   
   };
   // logout functions for expermintal purpose
   // const out =()=>{
@@ -57,6 +73,10 @@ function Google() {
       <button className="google_button" onClick={handleCal}>
         <img src={googleIcon} className="google_login_button_img"></img>
         <p>Login With Google</p>
+        {/* <p>First Name :{data.BZ}</p>
+    <p>Last Name :{data.RX}</p>
+    <p>email :{data.dw}</p>
+    <p>Profile Link :{data.yO}</p> */}
       </button>
     </>
   );
